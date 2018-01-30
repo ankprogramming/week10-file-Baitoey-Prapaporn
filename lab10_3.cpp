601492eb98fe90e17fc9a4d9cc1a7acf12e6e796
Unknown <toey.prapaporn123@gmail.com>
2018-01-30 12:20:12 +0700

    ทำเสร็จแล้ว

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,4 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 int main() {
-        /* code */
-        return 0;
+    int cmd;
+    string name;
+    string line;
+    ofstream writefile;
+    ifstream readfile;
+
+    do {
+        cout<<"MENU"<<endl;
+        cout<<"1.Add Student."<<endl;
+        cout<<"2.List Student."<<endl;
+        cout<<"3.Quit."<<endl;
+        cout<<"Please Enter Menu"<<endl;
+        cin>>cmd;
+
+        if (cmd == 1){
+            cout <<"Enter Student Name : ";
+            cin>>name;
+            writefile.open("std.txt",fstream::app);
+
+            if (writefile.is_open()){
+                writefile<<name<<endl;
+                writefile.close();
+            }else {
+                    cout<<"Unable to open file";
+            }
+        }else if (cmd==2){
+                    readfile.open("std.txt");
+                    if (readfile.is_open()){
+                        while(getline(readfile,line)){
+                            cout<<line<<endl;
+                        }
+                        readfile.close();
+                    }
+        }else{
+        break;
+        }
+    }while(1);
+    return 0;
+
 }
